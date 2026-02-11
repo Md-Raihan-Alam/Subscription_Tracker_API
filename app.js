97921849cb886bf936baeedde90ec368a0f5bbcb
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-02-11 11:44:10 +0600

    feat(routes,models): models & routes added

---
+++ app.js
@@ -0,0 +1,23 @@
+import e from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
+const app = e();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the subscription tracker api");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+  await connectToDatabase();
+});
+
+export default app;
---
+++ routes/subscription.routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "Get all subscriptions" })
+);
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "Get subscription detilas" })
+);
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ title: "Create subscriptions" })
+);
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: "Update subscriptions" })
+);
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: "delte a subscriptions" })
+);
+
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: "get all users  subscriptions" })
+);
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "cancel subscriptions" })
+);
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "Get upcoming renewals" })
+);
+export default subscriptionRouter;
