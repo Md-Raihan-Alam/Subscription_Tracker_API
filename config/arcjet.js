8c0c01c0cf677be8917bd6b5093f7681e243e44f
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-06-25 23:29:34 +0600

    fix: correct arcjet import to use @arcjet/node package

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,29 +1,15 @@
-import arcjet, { shield, detectBot, tokenBucket } from "arcjet";
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
 import { ARCJET_KEY } from "./env.js";
 
 const aj = arcjet({
   key: ARCJET_KEY,
-
-  // REQUIRED
-  client: {
-    name: "subscription-tracker-api", // any identifier for your app
-  },
-
-  // REQUIRED
-  log: {
-    level: "info",
-  },
-
   characteristics: ["ip.src"],
-
   rules: [
     shield({ mode: "LIVE" }),
-
     detectBot({
       mode: "LIVE",
       allow: ["CATEGORY:SEARCH_ENGINE"],
     }),
-
     tokenBucket({
       mode: "LIVE",
       refillRate: 5,
