440f7b6ef2e04c5f7c60d0c37c9e29fbf91cbb20
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-06-26 17:59:36 +0600

    feat: fix missing SERVER_URL import and add CSV export feature to subscription controller

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,5 +1,6 @@
 import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -14,7 +15,6 @@ export const createSubscription = async (req, res, next) => {
       headers: {
         "Content-Type": "application/json",
       },
-
       retries: 0,
     });
 
@@ -31,11 +31,66 @@ export const getUserSubscription = async (req, res, next) => {
   try {
     if (req.user.id !== req.params.id) {
       const error = new Error("You are not the owner of this account");
-      error.status = 401;
+      error.statusCode = 401;
       throw error;
     }
     const subscriptions = await Subscription.find({ user: req.params.id });
     res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const exportUserSubscriptionsCSV = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    if (!subscriptions.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No subscriptions found for this user",
+      });
+    }
+
+    const headers = [
+      "Name",
+      "Price",
+      "Currency",
+      "Frequency",
+      "Category",
+      "Payment Method",
+      "Status",
+      "Start Date",
+      "Renewal Date",
+    ];
+
+    const rows = subscriptions.map((s) => [
+      s.name,
+      s.price,
+      s.currency,
+      s.frequency,
+      s.category || "N/A",
+      s.paymentMethod,
+      s.status,
+      s.startDate ? s.startDate.toISOString().split("T")[0] : "N/A",
+      s.renewalDate ? s.renewalDate.toISOString().split("T")[0] : "N/A",
+    ]);
+
+    const csv = [headers, ...rows].map((row) => row.join(",")).join("\n");
+
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="subscriptions-${req.params.id}.csv"`
+    );
+
+    res.status(200).send(csv);
   } catch (e) {
     next(e);
   }
