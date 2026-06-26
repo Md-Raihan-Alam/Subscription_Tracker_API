01cf3ed05e22dd47c09e324f4d777237636db543
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2026-06-26 18:07:07 +0600

    feat: add Swagger JSDoc annotations to workflow routes

--- routes/workflow.routes.js
+++ routes/workflow.routes.js
@@ -1,7 +1,38 @@
 import { Router } from "express";
 import { sendReminders } from "../controllers/workflow.controller.js";
+
 const workflowRouter = Router();
 
-workflowRouter.post("/workflows/reminder", sendReminders);
+/**
+ * @swagger
+ * tags:
+ *   name: Workflows
+ *   description: Upstash QStash workflow endpoints
+ */
+
+/**
+ * @swagger
+ * /api/v1/workflows/reminder:
+ *   post:
+ *     summary: Trigger subscription renewal reminder workflow
+ *     tags: [Workflows]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [subscriptionId]
+ *             properties:
+ *               subscriptionId:
+ *                 type: string
+ *                 example: 64f1a2b3c4d5e6f7a8b9c0d1
+ *     responses:
+ *       200:
+ *         description: Workflow triggered successfully
+ *       400:
+ *         description: Invalid request
+ */
+workflowRouter.post("/reminder", sendReminders);
 
 export default workflowRouter;
